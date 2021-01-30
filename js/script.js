d905f2fa8a7570f1df07be1b03640c86d27cd155
Pavel Makarov <makarovpavel@hotmail.com>
2021-01-30 16:24:52 +0500

    home work 3

--- js/script.js
+++ js/script.js
@@ -2,20 +2,28 @@
 
 /* Задание на урок:
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+1) Первую часть задания повторить по уроку
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-Привет
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-4) Потренироваться и переписать цикл еще двумя способами*/
+P.S. Функции вызывать не обязательно*/
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -25,34 +33,65 @@ const personalMovieDB = {
   private: false,
 };
 
-let count = 0;
-while (count < 2) {
-  const firstQuestion = prompt("Один из последних просмотренных фильмов?");
-  const secondQuestion = prompt("На сколько оцените его?");
-  if (
-    firstQuestion != null &&
-    secondQuestion != null &&
-    firstQuestion != "" &&
-    secondQuestion != "" &&
-    firstQuestion.length < 50 &&
-    secondQuestion.length < 50
-  ) {
-    personalMovieDB.movies[firstQuestion] = secondQuestion;
-  } else {
-    count--; // Возврат итерации в начало цикла
+function rememberMyFilms() {
+  let count = 0;
+  while (count < 2) {
+    const firstQuestion = prompt("Один из последних просмотренных фильмов?");
+    const secondQuestion = prompt("На сколько оцените его?");
+    if (
+      firstQuestion != null &&
+      secondQuestion != null &&
+      firstQuestion != "" &&
+      secondQuestion != "" &&
+      firstQuestion.length < 50 &&
+      secondQuestion.length < 50
+    ) {
+      personalMovieDB.movies[firstQuestion] = secondQuestion;
+    } else {
+      count--; // Возврат итерации в начало цикла
+    }
+
+    count++;
   }
-
-  count++;
 }
 
-if (personalMovieDB.count === 10) {
-  console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-  console.log("Вы киноман");
-} else {
-  console.log("Произошла ошибка");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count === 10) {
+    console.log("Просмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+  } else {
+    console.log("Произошла ошибка");
+  }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyDB() {
+  if (personalMovieDB.private === false) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    const genreQuestion = prompt(`Ваш любимый жанр под номером ${i}`);
+    if (
+      genreQuestion != null &&
+      genreQuestion != "" &&
+      genreQuestion.length < 50
+    ) {
+      personalMovieDB.genres.push(genreQuestion);
+    } else {
+      i--; // Возврат итерации в начало цикла
+    }
+  }
+}
+
+writeYourGenres();
